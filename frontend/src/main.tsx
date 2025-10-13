08e35e3ba445754547d36f0c58f94a608b63aa28
nalatoli <nalatoli@nanotronics.co>
2025-10-13 16:39:34 -0400

    Remove eruda

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import eruda from "eruda";
-
-if (import.meta.env.DEV) {
-  eruda.init();
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
